4e5ed8cf43ad4d3b942f4c9f9631c1cfcbbb6e1d
Nirbhay Shegale <shegalenirbhay@gmail.com>
2025-09-25 17:23:17 +0530

    Update ShopCategory.jsx

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
-import './CSS/ShopCategory.css'
-import { useContext } from 'react'
-import { ShopContext } from '../Context/Shopcontext'
-import dropdown_icon from '../Assets/Frontend_Assets/dropdown_icon.png'
-import Items from '../Components/Items/Items'
-const ShopCategory = (props) => {
-  const {all_product}=useContext(ShopContext);
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+
+function App() {
   return (
-    <div className='shop-category'>
-      <img src={props.banner} alt="" />
-      <div className="shopcategory-indexsort">
-        <p>
-          <span>Showing 1-12</span> out of 36 Products
-        </p>
-        <div className="shopcategory-sort">
-          Sort by <img src={dropdown_icon} alt="" />
-        </div>
-      </div>
-      <div className="category-items">
-        {all_product.map((item,i)=>{
-          if(item.category===props.category){ 
-          return(
-            <Items key={i}
-            id={item.id} 
-            name={item.name} 
-            image={item.image}  
-            new_price={item.new_price} 
-            old_price={item.old_price} />
-          )
-        }
-        else{
-            return null
-            }
-        
-        })}
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Shop />} />
 
-      </div>
-      <div className="loadmore">
-        Load More
-        <img src={dropdown_icon} alt="" />
-      </div>
+        {/* Shop Categories */}
+        <Route
+          path="/men"
+          element={<ShopCategory banner="/banners/men.png" category="men" />}
+        />
+        <Route
+          path="/women"
+          element={<ShopCategory banner="/banners/women.png" category="women" />}
+        />
+        <Route
+          path="/kids"
+          element={<ShopCategory banner="/banners/kids.png" category="kids" />}
+        />
 
-    </div>
-  )
+        {/* Product Page */}
+        <Route path="/product/:productId" element={<Product />} />
+
+        {/* Cart */}
+        <Route path="/cart" element={<Cart />} />
+
+        {/* Login/Signup */}
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default ShopCategory
+export default App;
